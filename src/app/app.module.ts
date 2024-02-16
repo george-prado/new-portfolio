100abea26f17a89da71958d2eb8b851ef26143f7
george-prado <gmeloprado@gmail.com>
2024-02-15 23:40:29 -0300

    feat: nav & hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HeroSectionComponent } from './components/herosection/herosection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
