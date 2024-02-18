94d031f158b2149398801ed87e4fe08d5a11c502
george-prado <gmeloprado@gmail.com>
2024-02-17 23:42:37 -0300

    tailwind init config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.ts',
+    './src/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
